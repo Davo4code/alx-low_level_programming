b404b04f2ffdc1f3bdd4942223ca9d684bebe8df
Davo4code <dawithailu91@gmail.com>
2022-04-10 05:29:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 90; c <= 120; c++)
+	{
+		putchar(c);
+	}
+	putchar(10);
+	return (0);
+}
