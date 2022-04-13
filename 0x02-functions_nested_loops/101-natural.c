0558809ce7ff30571ca408c1872f69d79dfb6cf4
Davo4code <dawithailu91@gmail.com>
2022-04-13 12:57:08 -0700

    task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,15 @@
 
 int main(void)
 {
-	int i, res;
+int i, res;
 
-	for (i = 0; i < 1024; i++)
-		if (i % 3 == 0 || i % 5 == 0)
-			res += i;
-	printf("%d\n", res);
-	return (0);
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+res += i;
+}
+
+printf("%d\n", res);
+
+return (0);
 }
