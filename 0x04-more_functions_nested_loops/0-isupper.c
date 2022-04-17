ef64a59dc70febcb5574d360453ca526d34b9396
Davo4code <dawithailu91@gmail.com>
2022-04-16 19:26:58 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,40 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- * _isupper - checkes upper case
- * @c: print int
- * Return: give 1 or 0
+ * print_to_98 - prints from a given number upto 98
+ * @n: the number to start from
+ *
+ * Return: Nothing, Void
  */
-
-int _isupper(int c)
+void print_to_98(int n)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d", n);
+			}
+		}
+	}
+	printf("\n");
 }
