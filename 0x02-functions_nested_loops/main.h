bdfaf2db9dab53c455a2788ce012b05b5c859fbe
Davo4code <101502965+Davo4code@users.noreply.github.com>
2022-04-18 17:29:01 +0300

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,8 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void print_alphabet(void);
-void Print_alphabet_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -12,6 +11,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_90(int n);
+void print_to_98(int n);
 void print_times_table(int n);
-#endif /* _MAIN_H */
+
+#endif
