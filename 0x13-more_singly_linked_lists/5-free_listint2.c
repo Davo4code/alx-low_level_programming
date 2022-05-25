cd88319c064b3d134eb397c057c765e992b7821b
Davo4code <dawithailu91@gmail.com>
2022-05-25 07:28:06 -0700

    task-5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * free_listint2 - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
+	{
+		return;
+	}
+
+	free(*head);
+	head = 0;
+}
+
+    © 2022 GitHub, Inc.
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+    Contact GitHub
+    Pricing
+    API
+    Training
+    Blog
+    About
+
+
